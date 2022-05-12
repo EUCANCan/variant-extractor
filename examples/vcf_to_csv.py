002e2c764851b04b86d4a04627b06bd7b4eecc1d
Rapsssito <contact@rodrigomartin.dev>
2022-05-12 13:51:52 +0200

    chore: Add brackets to example vcf_to_csv

--- examples/vcf_to_csv.py
+++ examples/vcf_to_csv.py
@@ -47,8 +47,25 @@ if __name__ == '__main__':
         # Get called EVENTTYPE or SVTYPE from INFO field
         type_called = variant_record.info['EVENTTYPE'] if 'EVENTTYPE' in variant_record.info else \
             variant_record.info['SVTYPE'] if 'SVTYPE' in variant_record.info else None
-        variants.append([start_chrom, start, end_chrom, end, ref, alt, length, type_inferred, type_called])
+        # Brackets
+        brackets = ''
+        if type_inferred == VariantType.DEL.name:
+            brackets = 'N['
+        elif type_inferred == VariantType.DUP.name:
+            brackets = ']N'
+        elif type_inferred == VariantType.INV.name:
+            prefix = 'N' if variant_record.alt_sv_bracket.prefix else ''
+            suffix = 'N' if variant_record.alt_sv_bracket.suffix else ''
+            brackets = prefix + variant_record.alt_sv_bracket.bracket + suffix
+        elif type_inferred == VariantType.TRN.name:
+            prefix = 'N' if variant_record.alt_sv_bracket.prefix else ''
+            suffix = 'N' if variant_record.alt_sv_bracket.suffix else ''
+            brackets = prefix + variant_record.alt_sv_bracket.bracket + variant_record.alt_sv_bracket.bracket + suffix
 
-    df = pd.DataFrame(variants, columns=['start_chrom', 'start',
-                      'end_chrom', 'end', 'ref', 'alt', 'length', 'type_inferred', 'type_called'])
-    df.to_csv(f'{args.output_file}', index=False)
+        variants.append([start_chrom, start, end_chrom, end, ref, alt, length,
+                        brackets, type_inferred, type_called, str(variant_record)])
+
+    df = pd.DataFrame(variants, columns=['start_chrom', 'start', 'end_chrom', 'end', 'ref',
+                      'alt', 'length', 'brackets', 'type_inferred', 'type_called', 'variant_record_str'])
+
+    df.drop(['variant_record_str'], axis=1).to_csv(f'{args.output_file}', index=False)
